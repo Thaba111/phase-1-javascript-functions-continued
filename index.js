a2d1e935d9d40c9f29ed1052d8c8274ed98f3376
Edith Thaba <writersherry5@gmail.com>
2023-12-14 02:48:47 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+// 1. Implement saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 2. Implement mondayWork function
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // 3. Implement wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    // This "inner" function takes a single parameter (default to "special")
+    return function(adjective = "special") {
+      // Return a String of the form "You are ..." with the adjective wrapped in visual flair
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Call examples
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing")); //=> "You are !!!amazing!!!"
+  
+  console.log(wrapAdjective("%")("a dedicated programmer")); //=> "You are %a dedicated programmer%!"
+  
